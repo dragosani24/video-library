0041bc893e3424f3d9602b21a7f672e06259b30d
Santiago AV <dev3@tropicalhouserecords.com>
2025-08-18 15:29:26 -0700

    feat(test): Add test for anime site repository

---
+++ src/test/java/com/example/animeapi/repository/AnimeSiteRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.example.animeapi.repository;
+
+import com.example.animeapi.model.AnimeSite;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class AnimeSiteRepositoryTest {
+
+    @Autowired
+    private AnimeSiteRepository animeSiteRepository;
+
+    @Test
+    public void testFindByCategory() {
+        AnimeSite animeSite = new AnimeSite("Test Name", "http://test.com", "Test Description", "Streaming", true);
+        animeSiteRepository.save(animeSite);
+
+        List<AnimeSite> foundSites = animeSiteRepository.findByCategory("Streaming");
+        assertEquals(1, foundSites.size());
+        assertEquals("Streaming", foundSites.get(0).getCategory());
+    }
+
+    @Test
+    public void testFindByIsActive() {
+        AnimeSite animeSite = new AnimeSite("Test Name", "http://test.com", "Test Description", "Streaming", true);
+        animeSiteRepository.save(animeSite);
+
+        List<AnimeSite> foundSites = animeSiteRepository.findByIsActive(true);
+        assertEquals(1, foundSites.size());
+        assertEquals(true, foundSites.get(0).getActive());
+    }
+}
